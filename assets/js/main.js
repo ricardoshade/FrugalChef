6b81e979ddf405aa89de815a38f6667eb759bcb2
Ownerman123 <charlesshumway1@gmail.com>
2024-04-11 11:19:50 -0600

    add function to build url and call api. as well as some comments for future work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,57 @@
 const appId = '196c237a';
 const appkey = '9d8fce1dbd4a46fa288a0e8aa92c11e6';
+const testModalObj = {
+
+    mealtime: "Dinner",
+    health: ['gluten-free']
+
+}
+
+function FetchRecipesAndDisplay(event){
+
+  
+ // create modalObj with modal inputs
+ const modalObj = event 
+
+const apiCall = BuildEdamamCall(modalObj);
 
 
-fetch('https://api.edamam.com/api/recipes/v2?type=any&app_id=196c237a&app_key=9d8fce1dbd4a46fa288a0e8aa92c11e6&&field=image&field=label&field=source&field=url&field=ingredients&field=totalTime&field=cuisineType&field=mealType').then(function (response){
+    fetch(apiCall).then(function (response){
+    
+        return response.json();
+    
+    }).then(function (data){
+        console.log(data);
+    // make cards that hold data
+    // store excess for later same interaction use
+    // tally ingredients of displayed recipes
+    // get costs of recipes
+    // total costs
+    // display shopping list with total cost
+    // displaycards
+    
+    
+    }).catch(error => console.log('fetch failed'));
+}
 
-    return response.json();
-
-}).then(function (data){
-    console.log(data);
-}).catch();
 
 
-function BuildEdamamCall(){
+function BuildEdamamCall(searchparamsobj){
 
-    const basecall = 'https://api.edamam.com/api/recipes/v2?type=any&app_id=196c237a&app_key=9d8fce1dbd4a46fa288a0e8aa92c11e6&field=label&field=source&field=url&field=ingredients&field=totalTime&field=cuisineType&field=mealType';
+    let basecall = `https://api.edamam.com/api/recipes/v2?type=any&app_id=${appId}&app_key=${appkey}&field=label&field=source&field=url&field=ingredients&field=totalTime&field=cuisineType&field=mealType&random=true`;
+    if (searchparamsobj.mealtime){
+       basecall = basecall.concat("&mealType=" + searchparamsobj.mealtime);
+    }
+    if(searchparamsobj.health){
+        for(let i = 0; i < searchparamsobj.health.length; i++){
+            
+            basecall = basecall.concat("&health=" + searchparamsobj.health[i]);
+
+        }
+    }
+
+    console.log(basecall);
+    return basecall;
     
 
 }
