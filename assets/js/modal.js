f1b8c7fd0f06938d822a1cb78a14df600c8eeecb
aiona <aionarae@users.noreply.github.com>
2024-04-16 20:48:16 -0600

    fix modal dark mode colors

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -20,9 +20,9 @@ function healthList() {
   for (var i = 0; i < healthListArr.length; i++) {
       var li = document.createElement("li");
       li.innerHTML = `
-          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-              <input id="checkbox-item-${i}" type="checkbox" value="${healthListArr[i]}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-              <label for="checkbox-item-${i}" class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">${healthListArr[i]}</label>
+          <div class="flex items-center p-2 rounded ">
+              <input id="checkbox-item-${i}" type="checkbox" value="${healthListArr[i]}" class="w-4 h-4 text-black-600 bg-gray-100 border-gray-700 rounded focus:ring-grey-500 dark:focus:ring-grey-600 dark:ring-offset-grey-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+              <label for="checkbox-item-${i}" class="w-full ms-2 text-sm font-medium text-black-900 rounded dark:text-black-300">${healthListArr[i]}</label>
           </div>
       `;
       healthList.appendChild(li);
